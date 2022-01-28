99c9267628d4973dc1b9e35f1baa49ce7772807a
shankar.jha <shankar.jha@avizva.com>
2022-01-29 02:11:03 +0530

    Added all the tools that were needed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-export const  sum = (a, b) => a + b;
+import EditorJS from '@editorjs/editorjs';
+import getToolConfig from './tools';
+import DragDrop from 'editorjs-drag-drop';
+import Undo from 'editorjs-undo';
+
+class Editor {
+  constructor(el, data, onPlayClick, tools, otherConfig) {
+    this.config = getToolConfig(data, el, onPlayClick, tools, otherConfig);
+    this.editor = null;
+    console.log('Editor constructor', this.config);
+  }
+
+  init() {
+    this.editor = new EditorJS({
+      ...this.config,
+    });
+    return this.editor;
+  }
+}
+export { Editor, DragDrop, Undo };
+export default Editor;
---
+++ src/tools.js
@@ -0,0 +1,125 @@
+import Header from '@editorjs/header';
+import SimpleImage from 'image-with-timestamp';
+import Warning from '@editorjs/warning';
+import Alert from 'editorjs-alert';
+import NestedList from '@editorjs/nested-list';
+import Checklist from '@editorjs/checklist';
+import Embed from '@editorjs/embed';
+import Table from 'editorjs-table';
+import CodeBox from '@bomdi/codebox';
+import Marker from '@editorjs/marker';
+import InlineTool from '@editorjs/inline-code';
+import Tooltip from 'editorjs-tooltip';
+import Underline from '@editorjs/underline';
+
+const getToolConfig = (data, el, onPlayClick, tools, otherConfig) => {
+  console.log('getToolConfig', data, el, onPlayClick, tools, otherConfig);
+  const config = {
+    time: new Date().getTime(),
+    /**
+     * Id of Element that should contain the Editor
+     */
+    holder: el,
+
+    autofocus: false,
+
+    data: data,
+
+    /**
+     * Available Tools list.
+     * Pass Tool's class or Settings object for each Tool you want to use
+     */
+    tools: {
+      ...tools,
+      image: {
+        class: SimpleImage,
+        shortcut: 'CMD+SHIFT+I',
+        config: {
+          onPlayClick,
+        },
+      },
+      header: {
+        class: Header,
+        inlineToolbar: true,
+        shortcut: 'CMD+SHIFT+H',
+        config: {
+          levels: [1, 2, 3, 4, 5, 6],
+          defaultLevel: 3,
+        },
+      },
+      warning: {
+        class: Warning,
+        inlineToolbar: true,
+        shortcut: 'CMD+SHIFT+W',
+        config: {
+          titlePlaceholder: 'Title',
+          messagePlaceholder: 'Message',
+        },
+      },
+      list: {
+        class: NestedList,
+        inlineToolbar: true,
+      },
+      alert: {
+        class: Alert,
+        inlineToolbar: true,
+        shortcut: 'CMD+SHIFT+A',
+        config: {
+          defaultType: 'primary',
+        },
+      },
+      checklist: {
+        class: Checklist,
+        inlineToolbar: true,
+      },
+      embed: {
+        class: Embed,
+        config: {
+          services: {
+            youtube: true,
+            coub: true,
+          },
+        },
+      },
+      table: {
+        class: Table,
+        inlineToolbar: true,
+        config: {
+          rows: 2,
+          cols: 3,
+        },
+      },
+      codebox: {
+        class: CodeBox,
+      },
+      marker: {
+        class: Marker,
+        shortcut: 'CMD+SHIFT+M',
+      },
+      tooltip: {
+        class: Tooltip,
+        shortcut: 'CMD+SHIFT+T',
+        config: {
+          holder: el,
+        },
+      },
+      underline: {
+        class: Underline,
+        shortcut: 'CMD+SHIFT+U',
+        config: {
+          holder: el,
+        },
+      },
+      inlineCode: {
+        class: InlineTool,
+        shortcut: 'CMD+SHIFT+C'
+      },
+
+      // ...
+    },
+    ...otherConfig,
+  };
+  return config;
+};
+
+export default getToolConfig;
